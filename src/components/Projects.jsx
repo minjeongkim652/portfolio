0a1eb811d4d1b9ce75f814ee3179d37c19aff9ac
Minjeong Kim <kmj020513@gmail.com>
2026-05-22 23:38:12 +0900

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -38,7 +38,7 @@ const projects = [
 id: 4,
     title: '포토폴리오 웹 사이트',
     description: 'React와 Node.js 기반으로 제작한 풀스택 포트폴리오 웹사이트',
-    about : '프로젝트 경험과 기술 스택, 그리고 개발자로서의 방향성을 효과적으로 전달하기 위해 제작한 개인 포트폴리오 웹사이트입니다. React 기반 컴포넌트 구조로 프론트엔드를 구현하고, Node.js와 Express를 활용해 방문자 수 API를 구축하였습니다. MongoDB Atlas를 연동하여 방문자 데이터를 저장하며, Vercel과 Render를 통해 프론트엔드와 백엔드를 각각 배포하였습니다. 또한 GitHub 기반 CI/CD 환경을 구성하여 코드 변경 시 자동 배포가 가능하도록 구현했습니다. 사용자 경험을 고려해 부드러운 스크롤 이동, 모달 UI, 반응형 레이아웃, 프로젝트 영상 및 EmailJS 기반 메일 전송 기능 등을 적용했습니다.',
+    about : '프로젝트 경험과 기술 스택, 그리고 개발자로서의 방향성을 효과적으로 전달하기 위해 제작한 개인 포트폴리오 웹사이트입니다. React 기반 컴포넌트 구조로 프론트엔드를 구현하고, Node.js와 Express를 활용해 방문자 수 API를 구축하였습니다. MongoDB Atlas를 연동하여 방문자 데이터를 저장하며, Vercel과 Render를 통해 프론트엔드와 백엔드를 각각 배포하였습니다. 또한 GitHub 기반 CI/CD 환경을 구성하여 코드 변경 시 자동 배포가 가능하도록 구현했습니다. ',
     image: '/4.png',
     // video: 음 나중에 추가
     stack: 'React / Node.js / Express / MongoDB Atlas / Axios / EmailJS / Vercel / Render / GitHub',
